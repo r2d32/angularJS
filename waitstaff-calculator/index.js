794bdd86aa63fed8e80d7c375062bead76d8e088
r3d32 <cagudo@lion.lmu.edu>
2014-07-02 19:01:55 +0200

    created files for waitstaff calculator

---
+++ waitstaff-calculator/index.js
@@ -0,0 +1,29 @@
+angular.module('myApp', [])
+.controller('FormCtrl', function($scope, $attrs) {
+
+    $scope.submit = function() {
+        $scope.submitted = !$scope.submitted;
+        $scope.male_name_s = $scope.male_name;
+        $scope.dirty_task_s = $scope.dirty_task;
+        $scope.obnoxious_celebrity_s = $scope.obnoxious_celebrity;
+        $scope.job_title_s = $scope.job_title;
+        $scope.celebrity_s = $scope.celebrity;
+        $scope.huge_number_s = $scope.huge_number;
+        $scope.tedious_task_s = $scope.tedious_task;
+        $scope.useless_skill_s = $scope.useless_skill;
+        $scope.adjective_s = $scope.adjective; 
+
+        $scope.male_name_s = "";
+        $scope.dirty_task = "";
+        $scope.obnoxious_celebrity = "";
+        $scope.job_title = "";
+        $scope.celebrity = "";
+        $scope.huge_number = "";
+        $scope.tedious_task = "";
+        $scope.useless_skill = "";
+        $scope.adjective = "";
+    };
+
+    $scope.numberLimitT = 's';
+    $scope.submitted = false;
+});
